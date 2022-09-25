bb0b30fe1411e88440eb72196b1e33e35c5ed515
Aness <fhx3000@gmail.com>
2022-09-25 15:06:13 +0200

    Some Changes

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ autoUpdater.logger = log
 
 
 let MainWindow
+let updateWindow
 console.log(logPath)
 const callMainWindow = async() => {
     MainWindow = new BrowserWindow({
@@ -78,9 +79,51 @@ const callMainWindow = async() => {
     })
 }
 
+function createUpdateWindow() {
+    updateWindow = new BrowserWindow({
+        width: 500,
+        height: 300,
+        webPreferences: {
+            preload: path.join(__dirname, './src/js/update.js'),
+            devTools: false,
+            sandbox: false
+        },
+        show: false,
+        resizable: false,
+        frame: false
+    })
+    updateWindow.loadFile('./src/update.html')
+    updateWindow.once('ready-to-show', updateWindow.show)
+
+    if (!isDev) {
+        globalShortcut.register('CommandOrControl+R', () => {
+
+        })
+        globalShortcut.register('CommandOrControl+Shift+R', () => {
+
+        })
+        globalShortcut.register('CommandOrControl+Shift+I', () => {
+
+        })
+        globalShortcut.register('F11', () => {
+
+        })
+    }
+    updateWindow.on('closed', () => {
+        updateWindow = null
+    })
+
+
+
+}
 
 app.whenReady().then(() => {
-    callMainWindow()
+    if (isDev) {
+        callMainWindow()
+    } else {
+        createUpdateWindow()
+        autoUpdater.checkForUpdatesAndNotify()
+    }
 
 
 
@@ -91,6 +134,47 @@ app.whenReady().then(() => {
     })
 })
 
+autoUpdater.on('checking-for-update', () => {
+    updateWindow.webContents.send('checkForUpdate')
+
+})
+autoUpdater.on('update-not-available', () => {
+
+
+    updateWindow.webContents.send('noUpdates')
+
+})
+ipc.on('updatedBack', () => {
+
+
+    updateWindow.close()
+    createWindow()
+})
+autoUpdater.on('update-available', () => {
+    log.info('update aviliable')
+    autoUpdater.autoDownload = false;
+    updateWindow.webContents.send('updateAviliable')
+})
+
+ipc.on('startDownload', () => {
+    autoUpdater.downloadUpdate()
+})
+autoUpdater.on('download-progress', (progressTrack) => {
+    log.info('download progress')
+    updateWindow.webContents.send('downloadProgress', progressTrack)
+    log.info(progressTrack.percent)
+})
+autoUpdater.on('error', (err) => {
+    log.info(err)
+    log.info('erorr')
+})
+
+autoUpdater.on('update-cancelled', () => {
+    log.info('cancled')
+})
+autoUpdater.on('update-downloaded', () => {
+    log.info('downloaded')
+})
 
 app.on('window-all-closed', function() {
     if (process.platform !== 'darwin') app.quit()
